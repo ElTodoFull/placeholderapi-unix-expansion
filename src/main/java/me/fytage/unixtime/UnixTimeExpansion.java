81fd2973a8f5e00ebe6ce6cf5d4997810580cca3
ElTodoFull <fullmaster.yt@gmail.com>
2025-10-02 22:20:43 -0300

    Update UnixTimeExpansion.java
    Forgot to update version. oops!

--- src/main/java/me/fytage/unixtime/UnixTimeExpansion.java
+++ src/main/java/me/fytage/unixtime/UnixTimeExpansion.java
@@ -23,7 +23,7 @@ public class UnixTimeExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.0.0";
+        return "1.1.0";
     }
 
     @Override
